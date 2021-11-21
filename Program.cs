9f5e5147b6eef3da3cceadf66f147a9ff5487b06
kaiorod <caiorodrigo2775@gmail.com>
2021-11-21 17:05:56 -0300

    Finaliza RetPreenchido

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, alt, lar;
+
+            Console.WriteLine("--- Tamanho do Retângulo ---");
+            inicio:
+            Console.Write("Largura..: ");
+            lar = int.Parse(Console.ReadLine());
+
+            Console.Write("Altura...: ");
+            alt = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            c = 1;
+            if(alt > 0 && alt <=10 && lar > 0 && lar <= 10)
+            {
+                while(c <= alt)
+                {
+                    string ret = string.Concat(Enumerable.Repeat("*",lar));
+                        Console.WriteLine(ret);
+
+                    c++;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Digite ambos valores entre 1 e 10");
+                Console.ResetColor();
+                goto inicio;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RetPreenchido
-Exiba um retângulo preenchido, com altura e largura digitados pelo usuário (entre 1 e 10).  Ex.:  Tamanho do retângulo: Largura..: 10 Altura...: 4  ********** ********** ********** **********
+Exiba um retângulo preenchido, com altura e largura digitados pelo usuário (entre 1 e 10).  
+Ex.:  
+Tamanho do retângulo: 
+Largura..: 10 
+Altura...: 4  
+********** 
+********** 
+********** 
+**********
